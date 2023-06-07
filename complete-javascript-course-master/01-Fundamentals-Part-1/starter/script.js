763e540e26e91c8d9221b6fe3331d27e2dda2ab1
Timofei Druzhinin <132019863+BlackMarketButter@users.noreply.github.com>
2023-06-06 21:48:07 -0700

    yeah, i can like commit stuff and stuff now.

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -411,4 +411,7 @@ let galonsPerYear = workMilesYear / milesPerGalon;
 console.log(`You'd use this many galons a year: ${galonsPerYear}`);
 console.log(`Costco galon cost would be: ${(galonsPerYear) * costcoGalon} wihout a membership. And ${(((galonsPerYear) * costcoGalon) + costcoMembership)}`);
 console.log(`Average galon cost would be: ${(galonsPerYear) * averageGalon}`);
+
+I can change stuff YO!
+
 */
