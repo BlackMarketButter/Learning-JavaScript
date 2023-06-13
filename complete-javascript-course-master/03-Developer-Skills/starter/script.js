08e41d59d78d7016956e1835e1d8ada03f935a55
BlackMarketButter <timofei.druzhinin@gmail.com>
2023-06-12 21:03:41 -0700

    (Forgot to save some work not sure what lesson)

--- complete-javascript-course-master/03-Developer-Skills/starter/script.js
+++ complete-javascript-course-master/03-Developer-Skills/starter/script.js
@@ -25,6 +25,7 @@ const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 
 */
 
+/*
 function calcTempAmp(temps) {
   for (let i = 0; i < temps.length; i++) {
     if (typeof temps[i] === 'string') {
@@ -55,3 +56,58 @@ const testArray1 = [23, 32, 31, 31, 23, 23, 12, 31, 23];
 const testArray2 = [87, 97, 89, 78, 97, 89, 78, 97, 89];
 
 console.log(arrMerger(testArray1, testArray2));
+*/
+
+/*
+function measureKelvin() {
+  const measurement = {
+    type: 'temp',
+    unit: 'celsius',
+    value: Number(prompt('degrees celsius')),
+  };
+
+  //   console.log(measurement);
+
+  //   console.log(measurement.value);
+  //   console.warn(measurement.value);
+  //   console.error(measurement.value);
+
+  const kelvin = measurement.value + 273;
+  return kelvin;
+}
+
+console.log(measureKelvin());
+*/
+
+/*
+function calcTempAmp(temps) {
+  for (let i = 0; i < temps.length; i++) {
+    if (typeof temps[i] === 'string') {
+      temps.splice(i, 1);
+    }
+  }
+  console.log(temps);
+  const maxValue = Math.max(...temps);
+  const minValue = Math.min(...temps);
+  return maxValue - minValue;
+}
+
+function arrMerger(arr1, arr2) {
+  const mergedArr = [];
+
+  for (let i = 0; i < arr1.length; i++) {
+    mergedArr.push(arr1[i]);
+  }
+
+  for (let i = 0; i < arr2.length; i++) {
+    mergedArr.push(arr2[i]);
+  }
+
+  return mergedArr;
+}
+
+const testArray1 = [23, 32, 31, 31, 23, 23, 12, 31, 23];
+const testArray2 = [87, 97, 89, 78, 97, 89, 78, 97, 89];
+
+console.log(arrMerger(testArray1, testArray2));
+*/
